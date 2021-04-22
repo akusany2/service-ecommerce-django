80c5816deba09bcfdfa34ddcc2b3843f81e67332
Ankit Kr <ankitkumar0411@gmail.com>
2021-04-22 17:50:24 +0100

    Update key

--- serviceecom/settings.py
+++ serviceecom/settings.py
@@ -12,7 +12,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l$t!-3))0)r^-knn&$$_ng1*t@nrogeyt5bug(xhhtx=6)$jxt'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
